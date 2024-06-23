3ed6469557832876be193b562a2a7330e93bc15c
TOURE Idrissa <idrisstoure180@gmail.com>
2024-06-23 11:19:40 +0100

    t commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bookRoute = require("./routes/book");
 const productRoute = require("./routes/product");
 const overviewRoute = require("./routes/overview");
 const serverless = require("serverless-http");
+const router = express.Router();
 
 dotenv.config();
 mongoose.set("strictQuery", false);
@@ -33,15 +34,22 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/blogs", blogRoute);
-app.use("/api/books", bookRoute);
-app.use("/api/products", productRoute);
-app.use("/api/overviews", overviewRoute);
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/blogs", blogRoute);
+// app.use("/api/books", bookRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/overviews", overviewRoute);
+
+
+router.get("/", (req, res) => {
+  res.send("App is running..");
+});
 
 // app.listen(process.env.PORT || 500, () => {
 //   console.log("Server is run");
 // });
+
+app.use("/.netlify/functions/app", router);
 
 module.exports.handler = serverless(app);
